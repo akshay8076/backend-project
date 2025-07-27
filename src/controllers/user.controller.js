e7cdf48f093cca006b11fb92251a3cfc4cab047e
akshay8076 <palakshay2003@gmail.com>
2025-07-27 17:16:55 +0530

    fix controller bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -95,7 +95,7 @@ const loginUser = asyncHandler(async(req,res) =>{
 
     const {email , username,password}=req.body;
 
-    if (!username || !email) {
+    if (!(username || email)) {
         throw new ApiError(400,"username or email is required")
     }
 
@@ -154,7 +154,7 @@ const logoutUser=asyncHandler(async(req,res)=>{
 
     return res.status(200).clearCookie("accessToken",options).clearCookie("refreshToken",options).json(
                                                                 new ApiResponse(200,{},"User logged out"))
-                                                                
+
 })
 
 export { registerUser, loginUser,logoutUser}
